4d9fccba9a00d3a31f459ad197e2634c9ba8432b
Micheal77Namma <micheal1namma@gmail.com>
2026-06-02 13:11:15 +0300

    fixes

--- script.js
+++ script.js
@@ -78,7 +78,7 @@
 
       // Add wave animation to "Hello"
       const helloText = wrap.querySelector("#hello-text");
-      const text = "Hello Cutie! 🥰💕";
+      const text = "Hello Cutie! ❤️";
       helloText.innerHTML = text.split("").map(char => 
         `<span>${char === " " ? "&nbsp;" : char}</span>`
       ).join("");
